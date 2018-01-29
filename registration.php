8c867d2013fd127f6fe23774d5b0dcdb7247574c
Maciej Sławik <maciekslawik@gmail.com>
2018-01-29 19:17:47 +0100

    Module registration

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * File: registration.php
+ *
+ * @author      Maciej Sławik <maciej.slawik@lizardmedia.pl>
+ * Github       https://github.com/maciejslawik
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'LizardMedia_Sample',
+    __DIR__
+);
