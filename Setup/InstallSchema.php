68f0dbb81d25c1ba17d3f156a8608ba0d2a73c21
Maciej Sławik <maciej.slawik@lizardmedia.pl>
2018-01-30 11:54:55 +0100

    Install schema script

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * File: InstallSchema.php
+ *
+ * @author      Maciej Sławik <maciej.slawik@lizardmedia.pl>
+ * Github:      https://github.com/maciejslawik
+ */
+
+namespace LizardMedia\Sample\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+//InstallSchema script is  run when module is enabled.
+//It is responsible for creating required database tables.
+//
+//If you need to insert custom data when installing your module
+//you should create another script, InstallData.
+
+/**
+ * Class InstallSchema
+ * @package LizardMedia\Sample\Setup
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+
+        //Creating a custom table
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('lizardmedia_sample')
+        )->addColumn(
+            'entity_id',
+            Table::TYPE_INTEGER,
+            100,
+            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+            'Entity id'
+        )->addColumn(
+            'title',
+            Table::TYPE_TEXT,
+            32,
+            ['nullable' => true],
+            'Title'
+        )->addColumn(
+            'description',
+            Table::TYPE_TEXT,
+            1000,
+            ['nullable' => true],
+            'Description'
+        )->addColumn(
+            'created_at',
+            Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => Table::TIMESTAMP_INIT],
+            'Create date'
+        )->setComment(
+            'LizardMedia_Sample table'
+        );
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+    }
+}
